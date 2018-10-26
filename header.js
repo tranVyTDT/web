1758c5a4d010646813f05f2dbd98beff75057291
tranVyTDT <lienminhttv@gmail.com>
2018-10-26 12:26:21 +0700

    web home
    edit web home

--- header.js
+++ header.js
@@ -3,9 +3,17 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
 	 var x = document.getElementById("brand");
+	 var myimg = document.getElementById('icon')
+	 var mylogin = document.getElementById('login')
     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
         x.style.display = "none";
+        document.getElementById("header1").style.fontSize = "35px";
+       myimg.style.height = "50px";
+		myimg.style.width = "50px";
     } else{
-    	  x.style.display = "block";
+    	 x.style.display = "block";
+    	 document.getElementById("header1").style.fontSize = "70px";
+    	 myimg.style.height = "100px";
+		myimg.style.width = "100px";
     }
 }
