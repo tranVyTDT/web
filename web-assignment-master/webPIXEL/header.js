1682365174aa1fb356685c7fbf9dd5ad22ec117e
tranVyTDT <lienminhttv@gmail.com>
2018-10-29 09:11:34 +0700

    edit header
    header

--- web-assignment-master/webPIXEL/header.js
+++ web-assignment-master/webPIXEL/header.js
@@ -28,4 +28,26 @@ function scrollFunction() {
          my.style.height = "100px";
         my.style.width = "100px";
     }
+	// When the user scrolls down 50px from the top of the document, resize the header's font size
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+     var x = document.getElementById("brand");
+     var my = document.getElementById('icon1')
+     var mylogin = document.getElementById('header1')
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        x.style.display = "none";
+         mylogin.style.height = "50px";
+        mylogin.style.width = "50px";
+		my.style.height = "50px";
+        my.style.width = "50px";
+    } else{
+         x.style.display = "block";
+         mylogin.style.height = "100px";
+        mylogin.style.width = "100px";
+		my.style.height = "100px";
+        my.style.width = "100px";
+    }
+    
+}
 }
