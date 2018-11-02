fc817512b3f7415d9c90d1eb122be45788b27f20
tranVyTDT <lienminhttv@gmail.com>
2018-11-02 15:14:03 +0700

    add upload
    user can write posts

--- signin.js
+++ signin.js
@@ -5,21 +5,11 @@ function openNav() {
 function closeNav() {
   document.getElementById("myNav").style.width = "0%";
 }
-$(function() {
+function openNav1() {
+  document.getElementById("myNav1").style.width = "100%";
+}
 
-    $('#login-form-link').click(function(e) {
-		$("#login-form").delay(100).fadeIn(100);
- 		$("#register-form").fadeOut(100);
-		$('#register-form-link').removeClass('active');
-		$(this).addClass('active');
-		e.preventDefault();
-	});
-	$('#register-form-link').click(function(e) {
-		$("#register-form").delay(100).fadeIn(100);
- 		$("#login-form").fadeOut(100);
-		$('#login-form-link').removeClass('active');
-		$(this).addClass('active');
-		e.preventDefault();
-	});
+function closeNav1() {
+  document.getElementById("myNav1").style.width = "0%";
+}
 
-});
